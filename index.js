7431410f73a2216ce003184c982abd5b8062efa0
saurav tiwari <sauravtiwari145@gmail.com>
2024-11-06 01:42:23 +0545

    added routes

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello")
+import express, { json } from "express"
+import appRouter from "./routes/index.js";
+
+const app = express();
+
+//middlewares
+app.use(express.json())
+
+
+app.use("api/v1/products", appRouter)
+
+const PORT = process.env.PORT || 5000;  
+app.listen(PORT, ()=>console.log("Server is running at port: ", PORT))
